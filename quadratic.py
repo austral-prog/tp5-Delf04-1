0ba1a3728d3340eed7221bbbf94b2eb18a7a81e6
Delfina Taira <dtaira@mail.austral.edu.ar>
2025-09-05 09:17:15 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,40 @@
 # Replace the "ANSWER HERE" for your answer
-
 def roots(a, b, c):
-    return "ANSWER HERE"
-
+    discr = b**2 - 4*a*c
+    if discr > 0:
+        root1 = (-b + discr**0.5) / (2*a)
+        root2 = (-b - discr**0.5) / (2*a)
+        return f"({root1}, {root2})"
+    elif discr == 0:
+        root = -b / (2*a)
+        return f"({root})"
+    else:
+        return f"( )"
 
 def value_y(a, b, c, x):
-    return "ANSWER HERE"
+    y = a*x**2 + b*x + c
+    return y
 
 
 def to_string(a, b, c):
-    return "ANSWER HERE"
-
+    if a != 0 and b != 0 and c != 0:
+        cuadratica = f"f(x) = {a} * X^2 + {b} * X + {c}"  
+    elif a == 0 and b != 0 and c != 0:
+        cuadratica = f"f(x) = {b} * X + {c}"
+    return cuadratica
+    
 
 def derivation(a, b, c):
-    return "ANSWER HERE"
+    if a != 0:
+        derivada = f"f'(x) = {2*a} * X + {b}"
+        return derivada
+    elif a == 0 and b != 0:
+        derivada = f"f'(x) = {b}"
+        return derivada
+    elif a == 0 and b == 0:
+        derivada = f"f'(x) = 0"
+        return derivada
+    else:
+        derivada = f"f'(x) = {2*a} * X"
+        return derivada
+
